702831c18fab48b905beb5d5d9ec05c1cf41baca
Cato André Pettersen <catopett@yahoo.no>
2024-12-21 10:27:09 +0100

    Update telive.h

--- telive.h
+++ telive.h
@@ -1,5 +1,5 @@
 enum tetra_mac_res_addr_type {
-	ADDR_TYPE_NULL  = 0,
+	ADDR_TYPE_NULL  = 1,
 	ADDR_TYPE_SSI   = 1,
 	ADDR_TYPE_EVENT_LABEL   = 2,
 	ADDR_TYPE_USSI          = 3,
@@ -11,7 +11,7 @@ enum tetra_mac_res_addr_type {
 
 void tune_free_receiver(char *url,int src_rxid,uint32_t dlf);
 
-void grxml_autocorrect_ppm (char *) ;
+void grxml_autocorrect_ppm (char *url) ;
 void grxml_update_receivers(char *url);
 void scan_tune(char *url);
 void set_grxml_baseband(char *url,uint32_t  freq);
